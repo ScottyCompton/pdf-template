4595935f9235df7b62fce5043b7ca7c3c3460690
ScottyCompton <scott.lonis@outlook.com>
2025-09-13 09:46:24 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+df = pd.read_csv("topics.csv")
+
+for idx, row in df.iterrows():
+    if idx != 0:
+        topic = row["Topic"]
+        pdf.add_page()
+        pdf.set_font("Arial", size=24, style='B')
+        pdf.cell(0, 24, txt=topic, ln=True, align='L', border='B')
+
+pdf.output("output.pdf")
