8f4c852f268eb58a76870e691f6413aa28455095
ScottyCompton <scott.lonis@outlook.com>
2025-09-13 09:54:30 -0700

    added page ranges

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ df = pd.read_csv("topics.csv")
 for idx, row in df.iterrows():
     if idx != 0:
         topic = row["Topic"]
+        pages = row["Pages"]
         pdf.add_page()
         pdf.set_font("Arial", size=24, style='B')
         pdf.cell(0, 24, txt=topic, ln=True, align='L', border='B')
+        for i in range(pages-1):
+            pdf.add_page()
 
 pdf.output("output.pdf")
